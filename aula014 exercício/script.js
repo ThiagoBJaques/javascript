e82c5fea215dc9437be5fbb4600914400fdadae1
ThiagoBJaques <thiagojnh@gmail.com>
2024-07-02 09:58:42 -0300

    ex014 concluido

--- aula014 exercício/script.js
+++ aula014 exercício/script.js
@@ -3,16 +3,23 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
+    var minu = data.getMinutes()
     
-    msg.innerHTML = `Agora são ${hora} horas.`
-    if (hora = >= 0 && hora < 12) { // if = Se Hora for maior ou igual a zero e menor que meio dia
+
+    msg.innerHTML = `Agora são ${hora}:${minu} minutos.`
+    if (hora >= 0 && hora < 12) { // if = Se Hora for maior ou igual a zero e menor que meio dia
         //Bom Dia!
-        img.src = 'manha'
+        img.src = './imagens/manha.jpg'
+        document.body.style.background = '#4A5928'
+
     } else if (hora >= 12 && hora <= 18) {   // else if Se não se for maior ou igual a meio dia e menor que dezoito.
         //Boa Tarde
-        img.src = 'tarde'
+        img.src = './imagens/tarde.jpg'
+        document.body.style.background = '#8C4820'
+
     } else {    // else Se não 
         //Boa Noite!
-        img.src = 'noite'
+        img.src = './imagens/noite.jpg'
+        document.body.style.background = '#012340'
     }
 }

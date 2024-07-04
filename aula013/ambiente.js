c80412adc0ddb6bbd161fc7490a280d5f19f78cd
ThiagoBJaques <thiagojnh@gmail.com>
2024-07-04 17:25:51 -0300

    aula014

---
+++ aula013/ambiente.js
@@ -0,0 +1,24 @@
+var c = 1   // Estrutura de repetições de teste lógico no final
+    do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+
+
+/*  Estrutura de repetições de teste lógico no início
+
+var c = 1
+while (c <= 3) {    //Enquanto o contador (c) for menor que 6X ele executa o comando.
+    console.log(`Passo ${c}`)
+    c++
+}
+
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
